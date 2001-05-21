28b9d18ff5b82a7669c5fed4d5c17d33fb6a69be
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2001-05-21 16:28:18 +0200

    initial commit of version 2001-05-21 16:28:18.000000000 +0200

---
+++ gltools.html-dir/images.pl
@@ -0,0 +1,16 @@
+# LaTeX2HTML 97.1 (release) (July 13th, 1997)
+# Associate images original text with physical files.
+
+
+$key = q/{_inline}wtimesh{_inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG WIDTH="43" HEIGHT="26" ALIGN="MIDDLE" BORDER="0"
+ SRC="img1.gif"
+ ALT="$w \times h$">|; 
+
+$key = q/{_inline}mboxttmin>mboxttmax{_inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG WIDTH="74" HEIGHT="24" ALIGN="MIDDLE" BORDER="0"
+ SRC="img2.gif"
+ ALT="$\mbox{\tt min}\gt\mbox{\tt max}$">|; 
+
+1;
+
